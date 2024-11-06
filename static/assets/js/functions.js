679dbcc9c06a5dd04e5f1e554a5648b5a88342db
ubgultra <155318917+ubgultra@users.noreply.github.com>
2024-11-06 09:13:10 -0500

    plz

--- static/assets/js/functions.js
+++ static/assets/js/functions.js
@@ -118,7 +118,7 @@ function tabCloak() {
 
 
 function disableTabCloak() {
-  var newTitle = 'Doge | V4';
+  var newTitle = 'Ultraprox';
   var newIcon = '/assets/img/doge.jpg';
 
   localStorage.setItem('tabTitle', newTitle);
